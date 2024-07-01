f53b6ba258aee16823015e50bee21632306026c6
Ismael <ismaelrobalino03@gmail.com>
2024-07-01 08:08:49 -0500

    Creando carpetas

---
+++ ChuckNorrisApp/Database/ChuckNorrisJokeDatabase.cs
@@ -0,0 +1,36 @@
+﻿using SQLite;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+public class ChuckNorrisJokeDatabase
+{
+    readonly SQLiteAsyncConnection _database;
+
+    public ChuckNorrisJokeDatabase(string dbPath)
+    {
+        _database = new SQLiteAsyncConnection(dbPath);
+        _database.CreateTableAsync<ChuckNorrisJoke>().Wait();
+    }
+
+    public Task<List<ChuckNorrisJoke>> GetJokesAsync()
+    {
+        return _database.Table<ChuckNorrisJoke>().ToListAsync();
+    }
+
+    public Task<int> SaveJokeAsync(ChuckNorrisJoke joke)
+    {
+        if (joke.ID != 0)
+        {
+            return _database.UpdateAsync(joke);
+        }
+        else
+        {
+            return _database.InsertAsync(joke);
+        }
+    }
+
+    public Task<int> DeleteJokeAsync(ChuckNorrisJoke joke)
+    {
+        return _database.DeleteAsync(joke);
+    }
+}
---
+++ ChuckNorrisApp/Models/ChuckNorrisJoke.cs
@@ -0,0 +1,9 @@
+﻿using SQLite;
+
+public class ChuckNorrisJoke
+{
+    [PrimaryKey, AutoIncrement]
+    public int ID { get; set; }
+    public string Joke { get; set; }
+    public string Code { get; set; }
+}
